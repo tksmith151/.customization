ed9d97056d066c2f15fdaef1c257bed7868237f1
tsmith <tsmith@isrl.byu.edu>
2018-07-19 10:24:15 -0700

    Decomposed Functionality of Prompt Command

--- bashrc/prompt.sh
+++ bashrc/prompt.sh
@@ -1,1 +1,32 @@
-PS1="\u@\h:\w > "
+# Color Pallete
+RESET='\033[0m'
+
+CYAN='\033[1;36m'
+YELLOW='\033[1;33m'
+
+BG_BLUE='\033[1;37;44m'
+BG_CYAN='\033[1;37;46m'
+BG_GREEN='\033[1;37;42m'
+BG_RED='\033[1;37;41m'
+BG_YELLOW='\033[1;37;43m'
+
+PROMPT_COMMAND=BuildPromptCommand
+
+BuildPromptCommand(){
+    PS1=''
+    BuildHeader
+    BuildSystemInformation
+    BuildPrompt
+}
+
+BuildHeader(){
+    PS1+=" \d \t \n"
+}
+
+BuildSystemInformation(){
+    PS1+=" \u@\h:\w\n"
+}
+
+BuildPrompt(){
+    PS1+=" > "
+}
