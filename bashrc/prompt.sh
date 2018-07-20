2f707a798efd69aaa346165a9f05e74637196016
tsmith <tsmith@isrl.byu.edu>
2018-07-20 09:43:46 -0700

    More Universal Format

--- bashrc/prompt.sh
+++ bashrc/prompt.sh
@@ -1,24 +1,22 @@
 # Color Pallete
-RESET_ESC='\[\033[0m\]'
-
+BLACK='\[\033[1;30m\]'
+BLUE='\[\033[1;34m\]'
 CYAN='\[\033[1;36m\]'
 YELLOW='\[\033[1;33m\]'
 
-BG_BLUE='\[\033[1;37;44m\]'
-BG_CYAN='\[\033[1;37;46m\]'
-BG_YELLOW='\[\033[1;37;43m\]'
+RESET_ESC='\[\033[0m\]'
+
+GREEN='\033[1;32m'
+RED='\033[1;31m'
 
 RESET='\033[0m'
-BG_GRAY='\033[1;37;47m'
-BG_GREEN='\033[1;37;42m'
-BG_RED='\033[1;37;41m'
 
 # State Variables
 FIRST_PROMPT=1
 AT_PROMPT=1
 unset AT_PROMPT
 
-trap BuildStart DEBUG
+trap BuildOutput DEBUG
 PROMPT_COMMAND=BuildPromptCommand
 
 BuildPromptCommand(){
@@ -26,7 +24,8 @@ BuildPromptCommand(){
     AT_PROMPT=1
 
     PS1=''
-    BuildHeader
+    BuildTime
+    BuildLocation
     BuildSystemInformation
     BuildPrompt
 
@@ -35,36 +34,52 @@ BuildPromptCommand(){
         return
     fi
 
-    BuildFooter
-}
-
-BuildHeader(){
-    PS1+="${BG_BLUE} \d \t ${RESET_ESC}\n"
-}
-
-BuildSystemInformation(){
-    PS1+="${BG_CYAN} @ ${RESET_ESC}${CYAN} \u@\h:\w${RESET_ESC}\n"
-}
-
-BuildPrompt(){
-    PS1+="${BG_YELLOW} > ${RESET_ESC}${YELLOW} "
-    PS2="${BG_YELLOW} > ${RESET_ESC}${YELLOW} "
+    BuildExitStatus
 }
 
 BuildStart(){
+    PS1+="${BLACK}( ${RESET_ESC}"
+}
+
+BuildConnection(){
+    PS1+="${BLACK} )-( ${RESET_ESC}"
+}
+
+BuildEnd(){
+    PS1+="${BLACK} )${RESET_ESC}"
+}
+
+BuildTime(){
+    PS1+="${BLUE}\d \t${RESET_ESC}"
+}
+
+BuildLocation(){
+    PS1+="${CYAN}\u@\h:\w${RESET_ESC}"
+}
+
+BuildSystemInformation(){
+    PS1+=""
+}
+
+BuildPrompt(){
+    PS1+="${BLACK}->${RESET_ESC}${YELLOW} "
+    PS2="${BLACK}|->${RESET_ESC}${YELLOW} "
+}
+
+BuildOutput(){
     if [ -z "$AT_PROMPT" ]; then
         return
     fi
     unset AT_PROMPT
 
-    printf "${BG_GRAY}                     ${RESET}\n"
+    printf "${RESET}"
 }
 
-BuildFooter(){
+BuildExitStatus(){
     if [ $EXIT != 0 ]; then
-        printf "${BG_RED}                     ${RESET}"
+        printf "${RED} Error ${EXIT} ${RESET}"
     else
-        printf "${BG_GREEN}                     ${RESET}"
+        printf "${GREEN} Success ${RESET}"
     fi
-    printf "\n\n\n\n"
+    printf ""
 }
