1eb2ad0c9d784c4d7919beb3b8333961dbd7bcf5
tsmith <tsmith@isrl.byu.edu>
2018-07-20 09:58:58 -0700

    Updated Some Visuals

--- bashrc/prompt.sh
+++ bashrc/prompt.sh
@@ -1,11 +1,12 @@
 # Color Pallete
-BLACK='\[\033[1;30m\]'
+BLACK_ESC='\[\033[1;30m\]'
 BLUE='\[\033[1;34m\]'
 CYAN='\[\033[1;36m\]'
 YELLOW='\[\033[1;33m\]'
 
 RESET_ESC='\[\033[0m\]'
 
+BLACK='\033[1;30m'
 GREEN='\033[1;32m'
 RED='\033[1;31m'
 
@@ -38,15 +39,15 @@ BuildPromptCommand(){
 }
 
 BuildStart(){
-    PS1+="${BLACK}( ${RESET_ESC}"
+    PS1+="${BLACK_ESC}|-( ${RESET_ESC}"
 }
 
 BuildConnection(){
-    PS1+="${BLACK} )-( ${RESET_ESC}"
+    PS1+="${BLACK_ESC} )-( ${RESET_ESC}"
 }
 
 BuildEnd(){
-    PS1+="${BLACK} )${RESET_ESC}"
+    PS1+="${BLACK_ESC} )${RESET_ESC}"
 }
 
 BuildTime(){
@@ -66,8 +67,8 @@ BuildSystemInformation(){
 
 BuildPrompt(){
     BuildEnd
-    PS1+="${BLACK}->${RESET_ESC}${YELLOW} "
-    PS2="${BLACK}|->${RESET_ESC}${YELLOW} "
+    PS1+="${BLACK_ESC}->${RESET_ESC}${YELLOW} "
+    PS2="${BLACK_ESC}|->${RESET_ESC}${YELLOW} "
 }
 
 BuildOutput(){
@@ -80,10 +81,11 @@ BuildOutput(){
 }
 
 BuildExitStatus(){
+    printf "${BLACK}|== ${RESET}"
     if [ $EXIT != 0 ]; then
-        printf "${RED} Error ${EXIT} ${RESET}"
+        printf "${RED}Error ${EXIT}${RESET}"
     else
-        printf "${GREEN} Success ${RESET}"
+        printf "${GREEN}Success${RESET}"
     fi
     printf "\n"
 }
