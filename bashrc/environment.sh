ffa697318b3af1715f274ed2c21678acb39af810
tsmith <tsmith@isrl.byu.edu>
2018-07-24 11:28:38 -0700

    Added HISTIGNORE

--- bashrc/environment.sh
+++ bashrc/environment.sh
@@ -1,6 +1,7 @@
 export GREP_COLOR="1;32"
 export HISTCONTROL=ignoreboth
-export HISTSIZE=5000
+export HISTSIZE=1000
 export HISTFILESIZE=10000
+export HISTIGNORE="history:hs"
 export LS_COLORS="di=1;36:ex=1;33:fi=1;37:ln=1;32:mi=1;31:or=1;31:"
 export PROMPT_SIZE="min"
