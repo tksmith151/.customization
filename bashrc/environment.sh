039e803bed2516bd1d093c0ad86fdd3a3fb5e044
tsmith <tsmith@isrl.byu.edu>
2018-07-24 08:43:14 -0700

    Fixed LS issue

--- bashrc/environment.sh
+++ bashrc/environment.sh
@@ -1,3 +1,3 @@
 export GREP_COLOR="1;32"
-export LS_COLORS="di=1;36:ex=1;33:fi=1;34:nl=1;32:mi=1;31:no=1;73:or=1;31:"
+export LS_COLORS="di=1;36:ex=1;33:fi=1;34:no=1;32:mi=1;31:no=1;73:or=1;31:"
 export PROMPT_SIZE="min"
